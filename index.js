2076326744a9daed39611f8e2927eda0cb093b76
Rich Wilson <richard.wilson@smgdesign.org>
2020-08-21 22:43:26 +0100

    new date object for parse

--- index.js
+++ index.js
@@ -35,12 +35,12 @@ exports.manage = async (event, context, callback) => {
     projectId,
   });
   if (message.payload.start_date) {
-    console.log('start date parsed', Date.parse(message.payload.start_date));
-    message.payload.start_date = Firestore.Timestamp.fromDate(Date.parse(message.payload.start_date));
+    console.log('start date parsed', new Date(Date.parse(message.payload.start_date)));
+    message.payload.start_date = Firestore.Timestamp.fromDate(new Date(Date.parse(message.payload.start_date)));
   }
   if (message.payload.end_date) {
-    console.log('end date parsed', Date.parse(message.payload.end_date));
-    message.payload.end_date = Firestore.Timestamp.fromDate(Date.parse(message.payload.end_date));
+    console.log('end date parsed', new Date(Date.parse(message.payload.end_date)));
+    message.payload.end_date = Firestore.Timestamp.fromDate(new Date(Date.parse(message.payload.end_date)));
   }
   switch (command) {
     case 'create':
